b53f16dc6da8f5116ae3be2c62a49188e3cde1bc
GlencySwetha <95336245+GlencySwetha@users.noreply.github.com>
2021-12-03 10:56:15 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("Welcome to Jenkins");
+        System.out.println("First Jenkins Project");
     }
 }
